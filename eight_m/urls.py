2697f59d21957fc8450e84b9c4fba7ff4d0d1f7c
satamas <se.atamas@gmail.com>
2016-03-03 19:39:02 +0300

    Add template view.

--- eight_m/urls.py
+++ eight_m/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 # from django.contrib import admin
+from django.views.generic.base import TemplateView
+
 from app.views import *
 
 urlpatterns = [
     url('stats', get_statistic),
-    url('winner', add_winner)
+    url('winner', add_winner),
+    url('^$', TemplateView.as_view(template_name="index.html")),
 ]
